d316f0f78b6d594b5d48c490261d071e93c771b5
sambhajitaur1 <83905297+sambhajitaur1@users.noreply.github.com>
2024-08-03 10:58:48 +0530

    initial commit

---
+++ src/main/java/com/sambhajicode/SambhajicodeApplication.java
@@ -0,0 +1,13 @@
+package com.sambhajicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambhajicodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SambhajicodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sambhajicode/SambhajicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sambhajicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SambhajicodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
